855e2a7a08807bbbf10104d3b611baf36a851848
diptarimba <vipalkuda@gmail.com>
2022-01-09 02:29:59 +0700

    Perbaikan ketiga

--- js/script.js
+++ js/script.js
@@ -18,53 +18,82 @@ async function getData(data) {
     }
 }
 
-getData('project').then(res => {
-    res.forEach(data => {
-        const element = document.createElement('div');
-        element.classList.add('col-lg-4', 'col-md-12', 'p-3', 'border-top', 'border-bottom');
-        element.innerHTML = `
-            <div class="row">
-                            <div class="col">
-                                <img src="${data.img}" alt="${data.nama}" class="img-fluid rounded box-shadow">
-                            </div>
-                            <div class="col pl-2">
-                                <h2 class="h4 gradient-text font-weight-medium">${data.nama}</h2>   
-                                <small class="tag">${data.tags[0]}</small>
-                                <small class="tag">${data.tags[1]}</small>
-                                <small class="tag">${data.tags[2]}</small>
-                            </div >
-                        </div >
-                        <p class="text-secondary mt-1 project-description">${data.desc}</p>
-                        <div class="row ml-3">
-                            <div class="row ">
-                                <a href="${data.link[1]}" target="_blank" rel="noopener noreferrer" class="btn btn-gradient"><i
-                                        class="fa fa-globe"></i> Demo</a>
-                            </div>
-                            <div class="row ml-4">
-                                <a href="${data.link[0]}" target="_blank" rel="noopener noreferrer" class="btn btn-secondary"><i
-                                        class="fab fa-github"></i> Github</a>
-                            </div>
+$(document).ready(() => {
 
+	//Get the button
+	let mybutton = document.getElementById("btn-back-to-top");
 
-                        </div>
-            `
-        projectContainer.appendChild(element);
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+	  scrollFunction();
+	};
 
+	function scrollFunction() {
+	  if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	  ) {
+		mybutton.style.display = "block";
+	  } else {
+		mybutton.style.display = "none";
+	  }
+	}
+	// When the user clicks on the button, scroll to the top of the document
+	mybutton.addEventListener("click", backToTop);
 
-    });
+	function backToTop() {
+	  document.body.scrollTop = 0;
+	  document.documentElement.scrollTop = 0;
+	}
 })
 
-getData('certificate').then(res => {
-    res.forEach(data => {
-        const element2 = document.createElement('div');
-        element2.classList.add('col-lg-4', 'col-md-12', 'p-3');
-        element2.innerHTML = `
-    <h2 class= "text-center text-secondary h4" > ${data.ket}.</h2>
-<img src="${data.img}" alt="${data.ket}"
-    class="img-fluid rounded  box-shadow">
+// getData('project').then(res => {
+    // res.forEach(data => {
+        // const element = document.createElement('div');
+        // element.classList.add('col-lg-4', 'col-md-12', 'p-3', 'border-top', 'border-bottom');
+        // element.innerHTML = `
+            // <div class="row">
+                            // <div class="col">
+                                // <img src="${data.img}" alt="${data.nama}" class="img-fluid rounded box-shadow">
+                            // </div>
+                            // <div class="col pl-2">
+                                // <h2 class="h4 gradient-text font-weight-medium">${data.nama}</h2>   
+                                // <small class="tag">${data.tags[0]}</small>
+                                // <small class="tag">${data.tags[1]}</small>
+                                // <small class="tag">${data.tags[2]}</small>
+                            // </div >
+                        // </div >
+                        // <p class="text-secondary mt-1 project-description">${data.desc}</p>
+                        // <div class="row ml-3">
+                            // <div class="row ">
+                                // <a href="${data.link[1]}" target="_blank" rel="noopener noreferrer" class="btn btn-gradient"><i
+                                        // class="fa fa-globe"></i> Demo</a>
+                            // </div>
+                            // <div class="row ml-4">
+                                // <a href="${data.link[0]}" target="_blank" rel="noopener noreferrer" class="btn btn-secondary"><i
+                                        // class="fab fa-github"></i> Github</a>
+                            // </div>
 
-    `
-        certificateContainer.appendChild(element2)
 
-    })
-})
+                        // </div>
+            // `
+        // projectContainer.appendChild(element);
+
+
+    // });
+// })
+
+// getData('certificate').then(res => {
+    // res.forEach(data => {
+        // const element2 = document.createElement('div');
+        // element2.classList.add('col-lg-4', 'col-md-12', 'p-3');
+        // element2.innerHTML = `
+    // <h2 class= "text-center text-secondary h4" > ${data.ket}.</h2>
+// <img src="${data.img}" alt="${data.ket}"
+    // class="img-fluid rounded  box-shadow">
+
+    // `
+        // certificateContainer.appendChild(element2)
+
+    // })
+// })
